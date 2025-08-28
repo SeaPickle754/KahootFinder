9a614ebb5694e4c3589c273626b966c7c3cb59b2
seapickle754 <uraniumpjs@gmail.com>
2025-08-27 18:25:21 -0600

    Working, optimized. Args fixed w quality of life too

--- v2python/main.py
+++ v2python/main.py
@@ -7,28 +7,41 @@ import requests
 import random
 # for args
 import sys
+
+# This will get changed later
+game_pin = 123456
+# also changed later
+NICKNAME = "lavachicken"
+# you guessed it
+PIN_COUNT = 50
+
+def setVarsFromArgs():
+    global NICKNAME
+    global PIN_COUNT
+    # sys.argv[0] is script name.
+    if sys.argv[1] == "help":
+        print("usage: python main.py NICKNAME PIN_COUNT")
+        print("defaults to 50 pins with the name \"lavachicken\"")
+        quit()
+    # i love python constants <3
+    print(f"using nickname {sys.argv[1]} with pin count {sys.argv[2]}")
+    NICKNAME = sys.argv[1]
+    try:
+        PIN_COUNT = int(sys.argv[2])
+    except:
+        print("ERROR! Pin Count not valid number")
+
+setVarsFromArgs()
+print("Creating window instance...")
 # Options to run headless (no GUI window). Remove if you want to see it.
 options = Options()
-# options.headless = True  # Uncomment to run without a window
+options.headless = True  # Uncomment to run without a window
 
 # Start Firefox WebDriver
 driver = webdriver.Firefox(options=options)
 # open kahoot in advance
 driver.get("https://kahoot.it")
 time.sleep(2)
-# Replace with the game PIN
-game_pin = 123456
-NICKNAME = "lavachicken"
-PIN_COUNT = 50
-
-def setVarsFromArgs():
-    if sys.argv[1] == "help":
-        print("usage: python main.py NICKNAME PIN_COUNT")
-        print("defaults to 50 pins with the name \"lavachicken\"")
-        quit()
-    # i love python constants <3
-    NICKNAME = sys.argv[1]
-    PIN_COUNT = sys.argv[2]
 
 # Check if a element exists by its class.
 # returns a boolean.
@@ -39,28 +52,46 @@ def checkIfElementExists(divclass):
     if isElementReturned == 0:
         return False
 
-setVarsFromArgs()
-
+print(f"Starting search 0/{PIN_COUNT}")
 for i in range(PIN_COUNT):
     game_pin = random.randint(100_000, 1_000_000)
     response = requests.get(f"https://kahoot.it/reserve/session/{game_pin}")
-    print(response.status_code)
     if response.status_code == 200:
-        print(f"GAME PIN FOUND: PIN {game_pin} IS VALID!!!!!")
+        print(f"GAME PIN FOUND: PIN {game_pin} IS VALID!!!!! {i}/{PIN_COUNT}")
         time.sleep(0.5)
         # this enters game pin automatically
-        pin_input = driver.find_element(By.NAME, "gameId")
+        
+        while(1):
+            try:
+                pin_input = driver.find_element(By.NAME, 'gameId')
+            except:
+                time.sleep(0.1)
+            else:
+                break
         pin_input.send_keys(game_pin)
         pin_input.send_keys(Keys.RETURN)
         print("Entering Pin....")
         #try entering nickname here
-        time.sleep(3)
-        name_input = driver.find_element(By.NAME, 'nickname')
+        time.sleep(0.5)
+        # if the nickname field hasn't loaded, then let it load until it is.
+        while(1):
+            try:
+                name_input = driver.find_element(By.NAME, 'nickname')
+            except:
+                time.sleep(0.1)
+            else:
+                break
         name_input.send_keys(NICKNAME)
         name_input.send_keys(Keys.RETURN)
         print("Entering Name....")
         # if the error bar pops up keep going
         if not checkIfElementExists("notification-bar__Text-sc-1e4wbj0-1 jkdbXz"):
+            
+            if driver.title == "You're In!" or driver.title == "Kahoot!":
+                print(f"Congrats! Pin {game_pin} worked!")
+                # iykyk
+                print("So long and thanks for all the fish. Quitting.")
+                quit()
             print("No luck, game is dead.")
             continue
         else:
@@ -68,10 +99,11 @@ for i in range(PIN_COUNT):
                 print(f"Congrats! Pin {game_pin} worked!")
                 # iykyk
                 print("So long and thanks for all the fish. Quitting.")
+                options.headless = False
                 quit()
 
     else:
-        print(f"pin {game_pin} is not valid")
+        print(f"pin {game_pin} is not valid. {i}/{PIN_COUNT}")
 
 print("No games found. Quitting.")
 driver.quit()
